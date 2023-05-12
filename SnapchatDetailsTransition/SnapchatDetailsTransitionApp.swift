7ce0c141d192c9ad8f11e09518f35a861584a612
Samuel Uyigue <Samuel.uyigue@gmail.com>
2023-05-12 07:34:45 -0500

    Initial Commit

---
+++ SnapchatDetailsTransition/SnapchatDetailsTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapchatDetailsTransitionApp.swift
+//  SnapchatDetailsTransition
+//
+//  Created by Osaretin Uyigue on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SnapchatDetailsTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
