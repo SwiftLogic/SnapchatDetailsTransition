e6ca5251fa4406cc3a56676591f9695dc3d544e0
Samuel Uyigue <Samuel.uyigue@gmail.com>
2023-05-12 07:52:42 -0500

    Project setup

---
+++ SnapchatDetailsTransition/View/HomeView.swift
@@ -0,0 +1,27 @@
+//
+//  HomeView.swift
+//  SnapchatDetailsTransition
+//
+//  Created by Osaretin Uyigue on 5/12/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            HeaderView()
+        }
+    }
+    
+    @ViewBuilder
+    private func HeaderView() -> some View {
+        
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
